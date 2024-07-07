5cc50ac95232ba5aa5f96a3c0080c3127318da6b
Jhony Sanchez <jhony.sanchez@correounivalle.edu.co>
2024-07-07 13:56:41 +0200

    Vignette was created

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Text data
+#'
+#' A template of text for testing the package
+#'
+#' @format ## `testData`
+#' Dataframe with some words
+#' @source <https://rmets.onlinelibrary.wiley.com/doi/full/10.1002/cli2.19>
+"testData"
